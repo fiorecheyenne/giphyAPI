ce9359f307e548cebe16150c36cf87e091c67597
Hecate <fioriecheyenne@gmail.com>
2019-03-20 06:26:36 -0700

    object working

--- logic.js
+++ logic.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
-
-});
-
-var topics = ["space", "bobs burgers", 'fluid painting', 'disney', 'sloth', 'koala', 'elephants', 'rick and morty', 'workout', 'aliens'];
-
-console.log(topics);
-makeButtons();
-function makeButtons() {
-    //uncertain if I want to empty or not but lets give it a go
-    $("#butt").empty();
-
-    // loop through topics array 
-    for (var i = 0; i < topics.length; i++) {
-
-        //makin' the buttons 
-        var btn = $("<button type='button'>");
-        btn.addClass("btn btn-outline-dark");
-        btn.attr("data-topic", topics[i]);
-        console.log(topics[i]);
-        btn.text(topics[i]);
-        $("#butt").append(btn);
+    gifApp.makeButtons();
+})
+var gifApp = {
+    topics: ['space', 'bobs burgers', 'fluid painting', 'disney', 'sloth', 'koala', 'elephants', 'rick and morty', 'workout', 'aliens'],
 
 
-    }
+    makeButtons: function () {
+        //uncertain if I want to empty or not but lets give it a go
+        $("#butt").empty();
 
-    console.log(topics);
+        // loop through topics array 
+        for (var i = 0; i < gifApp.topics.length; i++) {
 
-}
+            //makin' the buttons 
+            var btn = $("<button>");
+            btn.addClass("btn btn-outline-dark");
+            btn.attr("data-topic", gifApp.topics[i]);
+
+            btn.text(gifApp.topics[i]);
+            $("#butt").append(btn);
+
+
+        }
+
+        console.log(gifApp.topics);
+
+    },
+};
